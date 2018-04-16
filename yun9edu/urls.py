1c0f02c7309b8fdfa5053ef088d0956f8ee8b016
woodshope <lanqy.zhang@gmail.com>
2018-04-16 16:55:33 +0800

    change url namespace weixinclient

--- yun9edu/urls.py
+++ yun9edu/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('clt/', include('weixinclient.urls',  namespace='weixinclient')),
+    path('clt/', include('weixinclient.urls')),
 ]
